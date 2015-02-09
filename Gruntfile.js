fd31db0c6e2658d38dd875a2de40aa4c895836b0
Prashanth <pgopi@yahoo-inc.com>
2015-02-09 00:16:04 -0800

    Adding files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,17 @@ module.exports = function(grunt) {
                 }
             }
         },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'results.txt', // Optionally capture the reporter output to a file
+                    quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['test/*.js']
+            }
+        },
         compass: {
             options: {
                 sassDir: 'css/sass',
